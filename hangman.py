eb84f5f46b0b957d9e5def19c5c084a4be2ac6f7
Ashley Rahl <asrahlcpt024@student.wethinkcode.co.za>
2026-06-02 16:00:48 +0200

    Check input is valid. Changed layout. added welcome message

--- hangman.py
+++ hangman.py
@@ -1,6 +1,6 @@
 import random
 
-# Get a random word from the list
+'''Get a random word from the list'''
 def get_random_word():
     # List of words to choose from
     # word_list = ["superman", "batman", "wonderwoman", "flash", "aquaman", "cyborg"]
@@ -12,8 +12,15 @@ def get_random_word():
     # return random.choice(word_list)
     return word
 
-# Play the game
+'''Welcome message and rules of the game'''
+def welcome_message():
+    print("\n---- Welcome to the Hangman Game ----\n")
+    print("The Rules: \n1. You have 6 lives to guess the word. \n2. Each time you guess a letter that is not in the word, you lose a life. \n3. The game ends when you either guess the word correctly or run out of lives. \n4. Good luck and have fun playing! \n")
+    print()
+
+'''Play the game'''
 def hangman():
+    # Get a random word from random function
     word = get_random_word()
     # Create a list of letters in the word
     letters_in_word = list(word)
@@ -22,38 +29,56 @@ def hangman():
     display = ["_" for _ in letters_in_word]
 
     # Keep track of what user already tried
-    guessed_letters = []
+    guessed_letters = [] # create an empty list to store guessed letters
+    guessed_letters.sort() # sort the guessed letters alphabetically for better readability
+    
     # User life attempts
     lives = 6
 
-    print(f"Your word contains {len(word)} letters. Here is the word: ")
-    print(display)
+    print(f"Your word contains {len(word)} letters.")
+    print(f"Here is the word: {' '.join(display)}\n")
     
     # Keep game in a loop as long as they have lives AND missing words ("_")
-    while "_" in display and lives >= 0:
-        print(f"\nLives left: {lives}")
-        print(f"Your guesses so far: {guessed_letters}\n")
+    while "_" in display and lives > 0:
+        print(f"Lives left: {lives}")
+        print(f"Your guesses so far: {' '.join(guessed_letters)}\n")
         # Get the user's guess
         guess = input("Guess a letter: ").lower()
 
+
+        # Check if the user has already guessed the letter
+        if guess in guessed_letters:
+            print(f"You've already guessed '{guess}'. Try a different letter.\n")
+            continue # skip the rest of the loop and prompt for another guess
+
+        # Check if the input is a single letter
+        if len(guess) != 1 or not guess.isalpha():
+            print("Invalid input. Please enter a single letter.\n")
+            continue
+
+        # Add the guessed letter to the list of guessed letters
         guessed_letters.append(guess) # add the guessed letter to the list of already guessed letters
+
+
         # Check if the guess is in the word
         if guess in letters_in_word:
             # If it is, replace the corresponding underscore with the letter
             for i in range(len(letters_in_word)):
                 if letters_in_word[i] == guess:
                     display[i] = guess
-            print(f"Correct! The word now looks like this: \n{display}")
+            print(f"Correct! The word updated: {' '.join(display)}\n")
         else:
             lives -= 1 # deduct a life 
-            print(f"Sorry, {guess} is not in the word. Try again.")
-            print(display)
+            print(f"Sorry! Try again. The word: {' '.join(display)}\n")
+            # print(f"")
 
+    # Check if the user has guessed the word correctly, congratulate them
     if "_" not in display:
         print("\nCongratulations! You've guessed the word correctly!")
     else:
         print(f"\nGame over! You've run out of lives. \nThe word was: {word}")
     
         
-
+''''Main function to run the game'''
+welcome_message()
 hangman()
