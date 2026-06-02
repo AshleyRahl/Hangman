7dc5c9481febe72475b5c3cf4052424019f950d0
Ashley Rahl <asrahlcpt024@student.wethinkcode.co.za>
2026-06-02 13:54:19 +0200

    created a function to check if the guesses letter in word and addeds the word to the correct index

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # Hangman
+
+Hangman is a word-guessing game
+Where a player needs to guess the secret word - one letter at a time.
--- hangman.py
+++ hangman.py
@@ -1,3 +1,29 @@
+
 # Startwith a word - for testing purposes
 word = "superman"
 
+# Create a list of letters in the word
+letters = list(word)
+# Create a list of underscores to represent the letters in the word
+display = ["_" for _ in letters]
+
+print(f"Your word contains {len(word)} letters. Here is the word: ")
+print(display)
+
+
+def hangman():
+    # Get the user's guess
+    guess = input("Guess a letter: ").lower()
+
+    # Check if the guess is in the word
+    if guess in letters:
+        # If it is, replace the corresponding underscore with the letter
+        for i in range(len(letters)):
+            if letters[i] == guess:
+                display[i] = guess
+        print(f"Correct! The word now looks like this: \n{display}")
+    else:
+        print(f"Sorry, {guess} is not in the word. Try again.")
+        print(display)
+
+hangman()
